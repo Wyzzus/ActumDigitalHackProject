7ef90130d2216c1883916cc9cf10d8303320a2b0
Wyzzus <wyzzus@gmail.com>
2018-05-20 01:35:43 +0300

    Create Admin Panel

---
+++ SocialNetworkActumHackAdmin/Assets/GetInfo.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class GetInfo : MonoBehaviour {
+
+    public string id;
+    public Text TF;
+    public RectTransform txt;
+    public RectTransform content;
+
+    public void StartWork()
+    {
+        StartCoroutine(LoadID());
+	}
+
+	void Update()
+	{
+        content.sizeDelta = new Vector2(content.sizeDelta.x, txt.sizeDelta.y + 20f);
+	}
+
+	IEnumerator LoadID()
+    {
+        id = TF.text;
+        WWW url = new WWW("https://api.fulldive.com/dive/" + id);
+        yield return url;
+        TF.text = url.text;
+        //Debug.Log(url.text);
+    }
+}
